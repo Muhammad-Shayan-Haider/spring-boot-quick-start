63a07c1c4ae2577d3c9a84ff75c4e6e6d2bcce1a
Muhammad Shayan Haider <shayanhaider1999@gmail.com>
2023-03-29 17:31:32 +0500

    add rest controller with a get method

---
+++ src/main/java/org/springbootstarter/hello/HelloController.java
@@ -0,0 +1,14 @@
+package org.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String sayHi() {
+        return "Hi";
+    }
+}

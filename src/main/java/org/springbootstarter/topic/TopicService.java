c335618264ecfd0267d4742aabe264137d794564
Muhammad Shayan Haider <shayanhaider1999@gmail.com>
2023-04-03 16:58:03 +0500

    add post request method

--- src/main/java/org/springbootstarter/topic/TopicService.java
+++ src/main/java/org/springbootstarter/topic/TopicService.java
@@ -2,16 +2,17 @@ package org.springbootstarter.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "Spring Framework", "Spring Framework Description"),
             new Topic("java", "Core Java", "Core Java Description"),
             new Topic("javascript", "Javascript", "Javascript Description")
-    );
+    ));
 
     public List<Topic> getTopics() {
         return topics;
@@ -19,5 +20,9 @@ public class TopicService {
 
     public Topic getTopic(String id) {
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
     }
 }

b8be9bae67dcc5f6cb31171ebff3b881c7a42fdb
Muhammad Shayan Haider <shayanhaider1999@gmail.com>
2023-04-10 17:16:06 +0500

    add course link

--- src/main/java/org/springbootstarter/topic/TopicController.java
+++ src/main/java/org/springbootstarter/topic/TopicController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Arrays;
 import java.util.List;
 
+// Course: https://www.youtube.com/watch?v=AI2oBJkPK3c&list=PLqq-6Pq4lTTbx8p2oCgcAQGQyqN8XeA1x&index=19
 @RestController
 public class TopicController {
 

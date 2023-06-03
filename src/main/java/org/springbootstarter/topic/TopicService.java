7cab172e119e4dfb5467be9a37b206d48c0e51e1
Muhammad Shayan Haider <shayanhaider1999@gmail.com>
2023-06-03 18:13:35 +0500

    make CRUD operations using repository

--- src/main/java/org/springbootstarter/topic/TopicService.java
+++ src/main/java/org/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package org.springbootstarter.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,43 +9,29 @@ import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-            new Topic("spring", "Spring Framework", "Spring Framework Description"),
-            new Topic("java", "Core Java", "Core Java Description"),
-            new Topic("javascript", "Javascript", "Javascript Description")
-    ));
+
+    @Autowired
+    private TopicRepository topicRepository;
 
     public List<Topic> getTopics() {
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);
         return topics;
     }
 
     public Topic getTopic(String id) {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+        return topicRepository.findById(id).get();
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic) {
-
-        for (int i = 0; i < topics.size(); i++) {
-            Topic t = topics.get(i);
-            if (t.getId().equals(id)) {
-                topics.set(i, topic);
-                return;
-            }
-        }
+        topicRepository.save(topic);
     }
 
     public void deleteTopic(String id) {
-
-        for (int i = 0; i < topics.size(); i++) {
-            Topic t = topics.get(i);
-            if (t.getId().equals(id)) {
-                topics.remove(i);
-                return;
-            }
-        }
+        topicRepository.deleteById(id);
     }
 }
